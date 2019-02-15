83dc41059901b80d732bced5fb9310285056c1b8
Piotr Zadka <piotr.zadka@gmail.com>
2019-02-15 22:14:50 +0000

    Working on adding Game to the database list

--- app/src/main/java/com/example/registrationactivity/game_list.java
+++ app/src/main/java/com/example/registrationactivity/game_list.java
@@ -1,8 +1,11 @@
 package com.example.registrationactivity;
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,21 +24,25 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 
 public class game_list extends AppCompatActivity {
 
     ArrayList gamesArray = new ArrayList();
     ArrayList platformArray = new ArrayList();
+    ArrayList gameDetails = new ArrayList();
     SearchView searchGame;
     Button clickMe ,exitButton;
     ListView gameList;
     ArrayAdapter adapter;
+    SessionManager sessionManager;
 
     // API FOR -> https://api.thegamesdb.net/key.php
-    String API_KEY = "ede83ff4569068653e175443c7380a8745cb63db71f81d3facbb09ada726eb39 ";  // PUBLIC
-   // String API_KEY = "eb8118a629a5c370b1ec29b2a9c6b24730daa44219efdc56756425747ba195fd "; // PRIVATE
+    String API_KEY = "ede83ff4569068653e175443c7380a8745cb63db71f81d3facbb09ada726eb39";  // PUBLIC
+   // String API_KEY = "eb8118a629a5c370b1ec29b2a9c6b24730daa44219efdc56756425747ba195fd"; // PRIVATE
 
 
 
@@ -50,6 +57,7 @@ public class game_list extends AppCompatActivity {
         searchGame = findViewById(R.id.searchGame);
         searchGame.setQueryHint("Search game");
         exitButton = findViewById(R.id.exitButton);
+        sessionManager = new SessionManager(this);
 
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +81,22 @@ public class game_list extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        gameList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                addGameToCollection();
+                String content = gameList.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+
+
     }
 
     // Browse online database for a specified game
@@ -93,17 +117,21 @@ public class game_list extends AppCompatActivity {
                             // Itterate through json object and add game titles to array
                             for(int i = 0; i<resultsArray.length(); i++){
                                 JSONObject game_title = resultsArray.getJSONObject(i);
+
                                 String platformID = game_title.get("platform").toString();
 
-                                gamesArray.add(game_title.getString("game_title")+", "+getPlatformName(platformID));
+                                //JSON needed for game details view (once clicked on list view item)
+                                //String gameDetailsJson = "{\"id\":\""+game_title.get("id")+"\", \"name\":\""+game_title.getString("game_title")+"\"}";
+                                //gameDetails.add(gameDetailsJson);
 
+
+                                gamesArray.add(game_title.getString("game_title")+", "+getPlatformName(platformID));
                             }
 
+                            System.out.println(gameDetails);
                             adapter = new ArrayAdapter(game_list.this,android.R.layout.simple_dropdown_item_1line,gamesArray);
                             gameList.setAdapter(adapter);
                             adapter.notifyDataSetChanged();
-
-
 
 
                         }catch (JSONException e) {
@@ -125,9 +153,7 @@ public class game_list extends AppCompatActivity {
     }
 
     private String getPlatformName(String id){
-        String name = "TEST";
-        System.out.println(platformArray);
-
+        String name = "";
         try{
 
             for(int i = 0; i < platformArray.size(); i++){
@@ -183,5 +209,47 @@ public class game_list extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
         return platformArray;
+    }
+
+    private void addGameToCollection(){
+        HashMap<String, String> user = sessionManager.getUserDetails();
+        final String id = user.get(sessionManager.ID);
+        String URL_DETAILS = "http://13.59.14.52/addGame.php";
+
+        StringRequest request = new StringRequest(Request.Method.POST, URL_DETAILS,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if(success.equals("1")){
+                                Toast.makeText(game_list.this,"Game Added!", Toast.LENGTH_LONG).show();
+                            }
+
+                        }catch (JSONException e){
+                            System.out.println(e);
+                            Toast.makeText(game_list.this,"Error adding game!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                })
+        {
+            protected Map<String, String> getParams(){
+
+                Map<String, String> params = new HashMap<>();
+                params.put("id", id);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
