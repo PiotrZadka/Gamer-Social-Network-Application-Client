ef5f1c80f072bf7a50dae9127ed98f5ec174e486
Piotr Zadka <piotr.zadka@gmail.com>
2019-04-11 16:03:57 +0100

    Commit after graduation
    Removed API info

--- app/src/main/java/com/example/registrationactivity/game_list.java
+++ app/src/main/java/com/example/registrationactivity/game_list.java
@@ -41,8 +41,7 @@ public class game_list extends AppCompatActivity {
     SessionManager sessionManager;
 
     // API FOR -> https://api.thegamesdb.net/key.php
-    //String API_KEY = "ede83ff4569068653e175443c7380a8745cb63db71f81d3facbb09ada726eb39";  // PUBLIC
-    String API_KEY = "eb8118a629a5c370b1ec29b2a9c6b24730daa44219efdc56756425747ba195fd"; // PRIVATE
+    String API_KEY = "insert api here"; // PRIVATE
 
 
 
